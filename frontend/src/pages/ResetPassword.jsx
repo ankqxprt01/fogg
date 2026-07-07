749617bcac161c3805c67d69df7ba5d302c20195
ankitgithub2101 <ankitquickxpert@gmail.com>
2026-07-07 23:09:34 +0530

    Update ResetPassword.jsx

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation,Navigate } from "react-router-dom";
 import API from "../api";
 
 function ResetPassword() {
