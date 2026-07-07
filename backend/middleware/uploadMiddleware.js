77fe6056fad752a7c3638c3cda41d7a30726b08e
ankitgithub2101 <ankitquickxpert@gmail.com>
2026-07-08 02:40:32 +0530

    Update uploadMiddleware.js

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,13 +1,19 @@
 const multer = require("multer");
 const path = require("path");
 
+
+const uploadDir = path.join(__dirname, "../uploads");
+
 // Storage config
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // folder to save uploaded files
+    cb(null, uploadDir);
   },
+
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9);
+
     cb(null, uniqueSuffix + path.extname(file.originalname));
   },
 });
